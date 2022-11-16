99e40d6588dda64c2204d66d81f3a79f0b5af0ba
TrickShotMLG02 <tim.schlachter@shinternet.ch>
2022-11-16 19:06:29 +0100

    Initial Commit of challenge-01 Files

---
+++ Challenge01/caesar.py
@@ -0,0 +1,41 @@
+# Name: 
+# Matriculation number: 
+
+def decrypt(cipher: str, shift: int) -> str:
+    '''
+    Args:
+        cipher: ciphertext
+        shift: shift used
+    Returns:
+        Decrypted plaintext
+    '''
+    possible_chars = [chr(i) for i in range(32, 127)]
+    pass
+
+def getShift(cipher: str, en_dictionary: str) -> int:
+    '''
+    Args:
+        cipher: ciphertext
+    Returns:
+        Shift that was used for this encryption
+    '''
+    possible_chars = [chr(i) for i in range(32, 127)]
+    pass
+
+
+if __name__ == "__main__":
+    with open("en_dictionary.txt", "r") as f:
+        en_dictionary = f.read()
+
+    cipher = ":NKeIGQKeOYeGeROKf"
+    expected_shift = 69
+    expected_plaintext = "The cake is a lie!"
+
+    shift = getShift(cipher, en_dictionary)
+    plaintext = decrypt(cipher, shift)
+
+    print(f"Ciphertext was:         {cipher}")
+    print(f"Guessed shift is:       {shift}")
+    print(f"Guessed plaintext is:   {plaintext}")
+    print(f"Shift worked:           {shift == expected_shift}")
+    print(f"Decrypt worked:         {plaintext == expected_plaintext}")
---
+++ Challenge01/frequency.py
@@ -0,0 +1,59 @@
+# Name: 
+# Matriculation number: 
+
+def countLetters(text: str) -> dict[str, int]:
+    '''
+    Args:
+        text: Self-explanatory
+
+    Returns:
+        Dictionary with number of occurrences for every letter (as lowercase)
+    '''
+    pass
+
+
+def convertToFrequency(letter_counts: dict[str, int]) -> dict[str, float]:
+    '''
+    Args:
+        letter_counts: Dictionary with number of occurrences for every letter (as lowercase)
+
+    Returns:
+        Dictionary with the frequency of occurrence for every letter (as lowercase)
+    '''
+    pass
+
+
+def plotLetterFreq(frequencies: dict[str, float], file=None) -> None:
+    '''
+    JUST FOR FUN! NOT IMPORTANT FOR THE TASK!
+    Args:
+        frequencies: Dictionary with the frequency of occurrence for every letter
+        file: [Optional] If specified, the graphic is not displayed, but saved in a file with this name
+    '''
+    try:
+        matplotlib = __import__("matplotlib", fromlist=["pyplot"])
+        plt = matplotlib.pyplot
+        labels = frequencies.keys()
+
+        plt.bar(range(len(labels)), frequencies.values(), tick_label=list(labels))
+        if file:
+            plt.savefig(file)
+        else:
+            plt.show()
+    except Exception as e:
+        print(f"An error occurred while plotting: {e}")
+        if "matplotlib" in str(e):
+            print(f"Is matplotlib installed? If not use 'pip install matplotlib' to install it!")
+
+
+if __name__ == "__main__":
+    text = "H3llo World!"
+    print(f"The text was:           {text}")
+
+    letter_counts = countLetters(text)
+    print(f"The letter counts are:  {letter_counts}")
+
+    frequencies = convertToFrequency(letter_counts)
+    print(f"The frequencies are:    {frequencies}")
+
+    # plotLetterFreq(frequencies)  # JUST FOR FUN! NOT IMPORTANT FOR THE TASK!
