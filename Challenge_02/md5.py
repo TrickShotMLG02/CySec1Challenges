c1257a475b5bc3799a37728504ed606d331c9de4
Tim Schlachter <tim.schlachter@shinternet.ch>
2022-11-25 13:44:11 +0100

    Defined buffer default values

--- Challenge_02/md5.py
+++ Challenge_02/md5.py
@@ -16,10 +16,10 @@ class MD5():
     BLOCK_SIZE = 64  # bytes (= 512 bits)
 
     # Initialize variables:
-    a0 = # TODO: Implement
-    b0 = # TODO: Implement
-    c0 = # TODO: Implement
-    d0 = # TODO: Implement
+    a0 = 0x67452301# TODO: Implement
+    b0 = 0xEFCDAB89# TODO: Implement
+    c0 = 0x98BADCFE# TODO: Implement
+    d0 = 0x10325476# TODO: Implement
 
     def loadState(self, hash):
         '''
