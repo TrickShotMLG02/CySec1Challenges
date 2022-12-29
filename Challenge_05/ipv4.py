2c6266afef936afc589b457c84c7d1fc724eed6d
TrickShotMLG02 <tim.schlachter@shinternet.ch>
2022-12-29 13:16:00 +0100

    Finished Task ipv4.py

--- Challenge_05/ipv4.py
+++ Challenge_05/ipv4.py
@@ -1,5 +1,7 @@
-import struct
+# Name: Tim Schlachter
+# Matriculation number: 7039326
 
+import struct
 
 def parseIPv4Header(header: bytes) -> tuple[list[int, int, int, int, int, int, int, int, int, int, int, int, int, bytes], bytes]:
     """
@@ -10,8 +12,23 @@ def parseIPv4Header(header: bytes) -> tuple[list[int, int, int, int, int, int, i
         ([version, header_length, type_of_service, total_length, identification, flag_mf, flag_df, fragment_offset,
             ttl, proto, checksum, source_addr, dest_addr, options_and_padding], payload)
     """
-    # TODO: Implement
-    pass
+    version = int.from_bytes(header[0:1], 'big') >> 4
+    header_length = int.from_bytes(header[0:1], 'big') & 0b00001111
+    type_of_service = int.from_bytes(header[1:2], 'big')
+    total_length = int.from_bytes(header[2:4], 'big')
+    identification = int.from_bytes(header[4:6], 'big')
+    flag_mf = (int.from_bytes(header[6:7], 'big') & 0b00100000) >> 5
+    flag_df = (int.from_bytes(header[6:7], 'big') & 0b01000000) >> 6
+    fragment_offset = (int.from_bytes(header[6:8], 'big') & 0b0001111111111111)
+    ttl = int.from_bytes(header[8:9], 'big')
+    proto = int.from_bytes(header[9:10], 'big')
+    checksum = int.from_bytes(header[10:12], 'big')
+    source_addr = int.from_bytes(header[12:16], 'big')
+    dest_addr = int.from_bytes(header[16:20], 'big')
+    options_and_padding = header[20:(20 + header_length * 4 - 20)]
+    payload = header[(20 + header_length * 4 - 20):]
+
+    return ([version, header_length, type_of_service, total_length, identification, flag_mf, flag_df, fragment_offset, ttl, proto, checksum, source_addr, dest_addr, options_and_padding], payload)
 
 
 def reassembleFragments(fragments: list[bytes]) -> bytes:
@@ -22,8 +39,16 @@ def reassembleFragments(fragments: list[bytes]) -> bytes:
     Returns:
         assembled payload
     """
-    # TODO: Implement
-    pass
+
+    # Since the offset is stored in index 7 of the parsed header, we sort the fragments by the index 7
+    sortedFragments = sorted(fragments, key=lambda x: x[7])
+
+    # Since the payload is in the second part of the touple we extract it by
+    resultingPayload = b''
+    for f in list(sortedFragments):
+        resultingPayload += parseIPv4Header(f)[1]
+
+    return resultingPayload
 
 
 if __name__ == "__main__":
